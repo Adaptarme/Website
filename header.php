876e40b0034e048e4fdc64ac715053a7467d82f7
felixricarb <felix.ricardo.barros@gmail.com>
2014-05-04 23:01:44 -0300

    Agregamos el favicon

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" />
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
