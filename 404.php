3eb322a6bfa3a2caf5591c07cbb43c446b42f049
felixricarb <felix.ricardo.barros@gmail.com>
2014-05-05 15:48:39 -0300

    Creamos la pagina para el error 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php
+get_header(); ?>
+	<div class="error">
+		<h3><?php _e('Ooops... Error 404'); ?></h3>
+		<h4><?php _e('Lo sentimos, pero la página que busca no existe.'); ?></h4>
+		<p>Revise la dirección introducida y vuelva a intentarlo o haz <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">clic aquí</a> para regresar.</p>
+	</div>
+<?php
+get_footer();
