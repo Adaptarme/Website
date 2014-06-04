e356f5c8b2dc72cd126e6e69bf97facc20bdc207
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-04 20:56:25 -0300

    Modificamos el texto del footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
             </div> 
             <footer class="footer" role="contentinfo">
-                <span>Desarrollado con <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/amor.png" title="amor"> en Chaco, Argentina.</span> 
+                <p>Desarrollado con <span title="Amor">&#10084;</span> por un grupo de geeks</p> 
             </footer>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
