f42725712d865d6464da0692e3caf99f3058b630
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-05 02:59:53 -0300

    Agregamos los enlaces sociales en el pie

--- footer.php
+++ footer.php
@@ -1,10 +1,16 @@
             </div> 
             <footer class="footer" role="contentinfo">
-                <p>Desarrollado con <span title="Amor">&#10084;</span> por un grupo de geeks • <a href="https://plus.google.com/113406858234515973523" rel="publisher">Google+</a></p> 
+            	<ul class="social list-inline">
+            		<li class="google"><a href="https://plus.google.com/113406858234515973523" rel="publisher">Google+</a></li>
+            		<li class="twitter"><a href="https://twitter.com/Adaptarme">Twitter</a></li>
+            		<li class="facebook"><a href="https://www.facebook.com/Adaptarme">Facebook</a></li>
+            		<li class="youtube"><a href="https://www.youtube.com/channel/UCchyiTcrctsH92crCQo0PmQ">YouTube</a></li>
+            	</ul>
+            	<p>Desarrollado con <span title="Amor">&#10084;</span> por Geeks</p>
             </footer>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+        <script src="//static.adaptarme.com.ar/bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
     </body>
 </html>
