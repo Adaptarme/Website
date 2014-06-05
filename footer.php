c3e64388bcaa34f5c972afbb54ed824726559f06
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-05 03:05:16 -0300

    Agregamos el target blank a los link

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
             </div> 
             <footer class="footer" role="contentinfo">
             	<ul class="social list-inline">
-            		<li class="google"><a href="https://plus.google.com/113406858234515973523" rel="publisher">Google+</a></li>
-            		<li class="twitter"><a href="https://twitter.com/Adaptarme">Twitter</a></li>
-            		<li class="facebook"><a href="https://www.facebook.com/Adaptarme">Facebook</a></li>
-            		<li class="youtube"><a href="https://www.youtube.com/channel/UCchyiTcrctsH92crCQo0PmQ">YouTube</a></li>
+            		<li class="google"><a href="https://plus.google.com/113406858234515973523" rel="publisher" target="_blank">Google+</a></li>
+            		<li class="twitter"><a href="https://twitter.com/Adaptarme" target="_blank">Twitter</a></li>
+            		<li class="facebook"><a href="https://www.facebook.com/Adaptarme" target="_blank">Facebook</a></li>
+            		<li class="youtube"><a href="https://www.youtube.com/channel/UCchyiTcrctsH92crCQo0PmQ" target="_blank">YouTube</a></li>
             	</ul>
             	<p>Desarrollado con <span title="Amor">&#10084;</span> por Geeks</p>
             </footer>
