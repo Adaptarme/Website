3f0046fd00cd55f05e202e3ddc279f5ac40fcd1f
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-05 02:29:16 -0300

    Eliminamos el script duplicado

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
             </div> 
             <footer class="footer" role="contentinfo">
-                <p>Desarrollado con <span title="Amor">&#10084;</span> por un grupo de geeks • <a href="https://plus.google.com/113406858234515973523" rel="publisher">Google+</a>
-</p> 
+                <p>Desarrollado con <span title="Amor">&#10084;</span> por un grupo de geeks • <a href="https://plus.google.com/113406858234515973523" rel="publisher">Google+</a></p> 
             </footer>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
--- header.php
+++ header.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="<?php echo get_bloginfo ( 'description' ); ?>">
         <title><?php wp_title(''); ?></title>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <meta name="description" content="<?php echo get_bloginfo ( 'description' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
         <link rel="stylesheet" href="//static.adaptarme.com.ar/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?=<?php echo rand(); ?>">
         <link rel="profile" href="http://gmpg.org/xfn/11">
