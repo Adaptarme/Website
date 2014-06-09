4bcbf2674c99e6991bb0640b73e707d2e5a66da0
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-09 18:44:35 -0300

    Refactorizamos

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <?php
 get_header(); ?>
 	<div class="error col-md-12">
-		<h3><?php _e('Ooops... Error 404'); ?></h3>
-		<h4><?php _e('Lo sentimos, pero la página que busca no existe.'); ?></h4>
+		<h3>Ooops... Error 404</h3>
+		<h4>Lo sentimos, pero la página que busca no existe.</h4>
 		<p>Revise la dirección introducida y vuelva a intentarlo o haz <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">clic aquí</a> para regresar.</p>
 	</div>
 <?php
