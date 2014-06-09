c4ec5cb4ec1335cb3e47e87359537bf988492a51
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-09 18:37:39 -0300

    Agregamos un padding a la pagina 404

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php
 get_header(); ?>
-	<div class="error">
+	<div class="error col-md-12">
 		<h3><?php _e('Ooops... Error 404'); ?></h3>
 		<h4><?php _e('Lo sentimos, pero la página que busca no existe.'); ?></h4>
 		<p>Revise la dirección introducida y vuelva a intentarlo o haz <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">clic aquí</a> para regresar.</p>
