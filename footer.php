da179834781d89624f85168f8992c1c32f3d7a99
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-12 20:04:43 -0300

    Movimos el footer al final

--- footer.php
+++ footer.php
@@ -7,18 +7,18 @@
  * @since Adaptar.ME 1.0
  */
 ?>
-    </div>
-    <footer class="footer" role="contentinfo">
-        <ul class="social list-inline">
-            <li class="google"><a href="https://plus.google.com/113406858234515973523" rel="publisher" target="_blank">Google+</a></li>
-            <li class="twitter"><a href="https://twitter.com/Adaptarme" target="_blank">Twitter</a></li>
-            <li class="facebook"><a href="https://www.facebook.com/Adaptarme" target="_blank">Facebook</a></li>
-            <li class="youtube"><a href="https://www.youtube.com/channel/UCchyiTcrctsH92crCQo0PmQ" target="_blank">YouTube</a></li>
-            <li class="github"><a href="https://github.com/Adaptarme" target="_blank">GitHub</a></li>
-        </ul>
-        <p>Desarrollado con <span title="Amor">&#10084;</span> por Geeks</p>
-    </footer>
-</div>
+    </div><!-- .row -->
+</div> <!-- .container -->
+<footer class="footer" role="contentinfo">
+    <ul class="social list-inline">
+    <li class="google"><a href="https://plus.google.com/113406858234515973523" rel="publisher" target="_blank">Google+</a></li>
+    <li class="twitter"><a href="https://twitter.com/Adaptarme" target="_blank">Twitter</a></li>
+    <li class="facebook"><a href="https://www.facebook.com/Adaptarme" target="_blank">Facebook</a></li>
+    <li class="youtube"><a href="https://www.youtube.com/channel/UCchyiTcrctsH92crCQo0PmQ" target="_blank">YouTube</a></li>
+    <li class="github"><a href="https://github.com/Adaptarme" target="_blank">GitHub</a></li>
+    </ul>
+    <p>Desarrollado con <span title="Amor">&#10084;</span> por Geeks</p>
+</footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
