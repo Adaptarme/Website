7aacd4da2ae2c5c14933eff41ee39086a91d2b9d
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-12 16:57:45 -0300

    Modificamos la ruta del js de boostrap

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
     </footer>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//static.adaptarme.com.ar/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
 </body>
 </html>
