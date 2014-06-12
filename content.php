e0ee35e22cf9f22bda5aa9f1b3978df3f704e1cd
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-12 14:58:22 -0300

    Quitamos el h2 de header

--- content.php
+++ content.php
@@ -12,7 +12,7 @@
 
 <article id="post-<?php the_ID(); ?>">
 	<?php if ( is_single() || is_category() || is_archive() ) : ?>
-		<?php the_title( '<h3>', '</h3>' ); ?>
+		<?php the_title( '<h2>', '</h2>' ); ?>
 		<?php the_content(); ?>
 	<?php else : ?>
 		<?php the_title( '<h4><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' ); ?>
