4191118a9ed25fe4be8669a5a9308d58afac865a
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-12 13:16:02 -0300

    Ordenamos el codigo

--- index.php
+++ index.php
@@ -8,20 +8,17 @@
  */
 
 get_header(); ?>
-    
-    <section role="main">
-        <div class="col-md-8">
-        <?php if ( have_posts() ) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'content', get_post_format() ); ?>
-            <?php endwhile; ?>
-        <?php else : ?>
-            <p>Lo sentimos, no hay posts publicados!</p>
-        <?php endif; ?>
-        </div>
-        <div class="col-md-4"></div>
+
+    <section class="col-md-8" role="main">
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'content', get_post_format() ); ?>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p>Lo sentimos, no hay posts publicados!</p>
+    <?php endif; ?>
     </section>
 
 <?php
-//get_sidebar();
+get_sidebar();
 get_footer();
--- sidebar.php
+++ sidebar.php
@@ -1,2 +1,3 @@
 <div class="sidebar col-md-4" role="complementary">
+	
 </div>
