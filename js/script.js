381193c90e1a00294a5f0123a2c595c40c34679f
felixricarb <felix.ricardo.barros@gmail.com>
2014-06-30 02:02:27 -0300

    Enviamos el formulario por AJAX

--- js/script.js
+++ js/script.js
@@ -1,50 +1,49 @@
 $(document).on('ready', function() {
 
-	$('#sendContact').on('click', function() {
-		
-		var ajaxUrl = 'wp-admin/admin-ajax.php';
-		var form = $('#formContact');
-		
-		form.validate({
-			rules: {
-				name: "required",
-				email: {
-					required: true,
-					email: true
-				},
-				content: {
-					required: true
-				}
+	var ajaxUrl = 'wp-admin/admin-ajax.php';
+	var form = $('#formContact');
+
+	form.validate({
+		rules: {
+			name: "required",
+			email: {
+				required: true,
+				email: true
 			},
-			messages: {
-				name: {
-					required: "Ingrese su nombre por favor.",
-				},
-				email: {
-					required: "Ingrese su correo.",
-					email: "El correo no es valido."
-				},
-				content: {
-					required: "Escriba el contenido del mensaje."
-				}
-			},
-			highlight: function(element) {
-				$(element).closest('.form-group').addClass('has-error');
-			},
-			unhighlight: function(element) {
-				$(element).closest('.form-group').removeClass('has-error');
-			},
-			errorElement: 'span',
-			errorClass: 'help-block',
-			errorPlacement: function(error, element) {
-				if(element.parent('.input-group').length) {
-					error.insertAfter(element.parent());
-				} else {
-					error.insertAfter(element);
-				}
+			content: {
+				required: true
 			}
-		});
-		
+		},
+		messages: {
+			name: {
+				required: "Ingrese su nombre por favor.",
+			},
+			email: {
+				required: "Ingrese su correo.",
+				email: "El correo no es valido."
+			},
+			content: {
+				required: "Escriba el contenido del mensaje."
+			}
+		},
+		highlight: function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		unhighlight: function(element) {
+			$(element).closest('.form-group').removeClass('has-error');
+		},
+		errorElement: 'span',
+		errorClass: 'help-block',
+		errorPlacement: function(error, element) {
+			if(element.parent('.input-group').length) {
+				error.insertAfter(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+	});
+
+	$('#sendContact').on('click', function() {
 		// Si el formulario es True
 		$.ajax({
 			type: "POST",
@@ -52,21 +51,17 @@ $(document).on('ready', function() {
 			url: ajaxUrl,
 			data: {
 				action: 'send_email',
-				name: 'Felix Barros',
-				email: 'felix.ricardo.barros@gmail.com',
-				content: 'Este es el cuerpo del mensaje.'
+				name: $('#name').val(),
+				email: $('#email').val(),
+				content: $('#content').val()
 			},
-			success: function(msg){
+			success: function(msg) {
 				alert(msg);
 			},
-			error: function(){
+			error: function() {
 				alert('error');
 			}
 		});
-
 	});
 
-
-
 });
-
