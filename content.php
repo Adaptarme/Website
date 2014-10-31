319be42329f6beb295fc3b004f8ac40ba0efc18b
Felix Barros <felix.ricardo.barros@gmail.com>
2014-10-31 12:20:42 -0300

    Eliminamos el archivo front-page

--- content.php
+++ content.php
@@ -15,12 +15,15 @@
 		<?php the_title( '<h2>', '</h2>' ); ?>
 		<?php the_content(); ?>
 	<?php else : ?>
-		<a href="<?php the_permalink(); ?>" rel="bookmark"><img src="http://demo.symphonythemes.com/drupal7/helpdesk/sites/demo.symphonythemes.com.drupal7.helpdesk/files/styles/blog-modern/public/field/image/dubai-69386.resized.jpg" class="img-responsive"></a>
-		<?php if ( has_post_thumbnail()) : ?>
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
-		<?php endif; ?>
-		<?php the_title( '<h4><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' ); ?>
-		<small> por <?php the_author(); ?></small>
-		<?php //the_excerpt(); ?>
+		<div class="views-row">
+			<a href="<?php the_permalink(); ?>" rel="bookmark"><img src="http://demo.symphonythemes.com/drupal7/helpdesk/sites/demo.symphonythemes.com.drupal7.helpdesk/files/styles/blog-modern/public/field/image/dubai-69386.resized.jpg" class="img-responsive"></a>
+			<div class="inner">
+			<?php if ( has_post_thumbnail()) : ?>
+				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
+			<?php endif; ?>
+				<?php the_title( '<h4><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' ); ?>
+				<?php //the_excerpt(); ?>
+			</div>
+		</div>
 	<?php endif; ?>
 </article>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * La pagina principal del sitio web
+ *
+ * @package WordPress
+ * @subpackage Adaptarme
+ * @since Adaptarme 1.0
+ */
+
+get_header(); ?>
+
+<aside class="container">
+	<div class="row"> 
+		<div class="services clearfix">
+			<h2>Diseñamos y desarrollamos.</h2>
+			<p class="lead">Somos un equipo de profesionales que se dedican al desarrollo de software y la capacitación en las últimas tecnologías.</p>
+			<div class="col-lg-4">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/easel.png" class="img-responsive">
+				<h3>Consultoría</h3>
+				<p>Especialistas altamente capacitados para asesorarlos en todo.</p>
+			</div>
+			<div class="col-lg-4">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/dev.png" class="img-responsive">
+				<h3>Desarrollo</h3>
+				<p>Desarrollamos soluciones profesionales, robustas y escalables.</p>
+			</div>
+			<div class="col-lg-4">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/bookshelf.png" class="img-responsive">
+				<h3>Capacitación</h3>
+				<p>Cursos para usuarios, directores de proyectos o diseñadores.</p>
+			</div>
+		</div>
+	</div><!-- .row -->
+</aside>
+
+<hr class="half-rule">
+
+<section class="container" role="main">
+	<div class="row">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<article class="col-md-4">
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		</article>
+		<?php endwhile; endif; ?>
+	</div><!-- .row -->
+</section>
+		
+<?php
+get_footer();
