14fcb01e9592b72c4abe569732cb67b3d2cb9e2f
Felix Barros <soyFelixBarros@gmail.com>
2015-01-07 01:13:21 -0300

    Modal contacto

--- js/script.js
+++ js/script.js
@@ -1,82 +1,83 @@
 // Load is used to ensure all images have been loaded, impossible with document
- 
+
 jQuery(window).load(function () {
 
 	/*----------------------------------------------------*/
-    /*  Contact Form
-    /*----------------------------------------------------*/
-	var ajaxUrl = 'wp-admin/admin-ajax.php';
-	var modal = $('#modal');
-	var form = $('#formContact');
-	var sendEmail = $('#sendEmail');
+	/*  Contact Form
+	/*----------------------------------------------------*/
+	$('#modal').on('shown.bs.modal', function () {
+		var ajaxUrl = 'wp-admin/admin-ajax.php';
+		var modal = $('#modal');
+		var form = $('#formContact');
+		var sendEmail = $('#sendEmail');
 
-	form.validate({
-		rules: {
-			name: 'required',
-			email: {
-				required: true,
-				email: true
-			},
-			content: {
-				required: true
-			}
-		},
-		messages: {
-			name: {
-				required: 'Ingrese su nombre por favor.',
-			},
-			email: {
-				required: 'Ingrese su correo.',
-				email: 'El correo no es valido.'
-			},
-			content: {
-				required: 'Escriba el contenido del mensaje.'
-			}
-		},
-		highlight: function (element) {
-			$(element).closest('.form-group').addClass('has-error');
-		},
-		unhighlight: function (element) {
-			$(element).closest('.form-group').removeClass('has-error');
-		},
-		errorElement: 'span',
-		errorClass: 'help-block',
-		errorPlacement: function (error, element) {
-			if(element.parent('.input-group').length) {
-				error.insertAfter(element.parent());
-			} else {
-				error.insertAfter(element);
-			}
-		}
-	});
-
-	$(form).on('submit', function (event) {
-    	event.preventDefault(); // Evitamos que el formulario se envie
-    	if (form.valid()) {
-    		sendEmail.button('loading');
-			$.ajax({
-				type: 'POST',
-				action: 'send_email',
-				url: ajaxUrl,
-				data: {
-					action: 'send_email',
-					name: $('#name').val(),
-					email: $('#email').val(),
-					content: $('#content').val()
+		form.validate({
+			rules: {
+				name: 'required',
+				email: {
+					required: true,
+					email: true
 				},
-				success: function (msg) {
-					var alert = '.alert';
-					$(form)[0].reset(); // Limpiamos los campos del form
-					sendEmail.button('reset'); // Reseteamos el texto del button
-					$(alert).html(msg).show();
-					setTimeout(function(){ $(alert).hide(); }, 5000);
-				},
-				error: function () {
-					alert('error');
+				content: {
+					required: true
 				}
-			});
-		}
+			},
+			messages: {
+				name: {
+					required: 'Ingrese su nombre por favor.',
+				},
+				email: {
+					required: 'Ingrese su correo.',
+					email: 'El correo no es valido.'
+				},
+				content: {
+					required: 'Escriba el contenido del mensaje.'
+				}
+			},
+			highlight: function (element) {
+				$(element).closest('.form-group').addClass('has-error');
+			},
+			unhighlight: function (element) {
+				$(element).closest('.form-group').removeClass('has-error');
+			},
+			errorElement: 'span',
+			errorClass: 'help-block',
+			errorPlacement: function (error, element) {
+				if(element.parent('.input-group').length) {
+					error.insertAfter(element.parent());
+				} else {
+					error.insertAfter(element);
+				}
+			}
+		});
 
+		$(form).on('submit', function (event) {
+			event.preventDefault(); // Evitamos que el formulario se envie
+			if (form.valid()) {
+				sendEmail.button('loading');
+				$.ajax({
+					type: 'POST',
+					action: 'send_email',
+					url: ajaxUrl,
+					data: {
+						action: 'send_email',
+						name: $('#name').val(),
+						email: $('#email').val(),
+						content: $('#content').val()
+					},
+					success: function (msg) {
+						$(form)[0].reset(); // Limpiamos los campos del form
+						sendEmail.button('reset'); // Reseteamos el texto del button
+						$('.alert').html(msg).show();
+						setTimeout(function () { $(alert).hide(); }, 5000);
+					},
+					error: function () {
+						alert('error');
+					}
+				});
+			}
+
+		});
 	});
 
 	// Masonry ***********************************************
@@ -84,7 +85,7 @@ jQuery(window).load(function () {
 	// Takes the gutter width from the bottom margin of .post
 	var gutter = parseInt(jQuery('.post').css('marginBottom'));
 	var container = jQuery('#posts');
- 
+
 	// Creates an instance of Masonry on #posts
 	container.masonry({
 		gutter: gutter,
@@ -122,11 +123,11 @@ jQuery(window).load(function () {
 			
 			//jQuery('#posts, #grid').css('width', posts_width);
 			jQuery('#grid').css({'margin': '0 auto'});
-        		
-		
-		
+
+
+
 		}
 	}).trigger('resize');
 
- 
+
 });
